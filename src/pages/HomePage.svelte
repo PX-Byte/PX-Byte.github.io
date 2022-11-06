eafaceb3359c107efa68e18c803e95d6c7e02404
KryptoPX <pixelbit2000@gmail.com>
2022-11-06 08:54:40 +0100

    🚚 move homepage, to pages folder

---
+++ src/pages/HomePage.svelte
@@ -0,0 +1,11 @@
+<h1>Welcome to PX-Byte</h1>
+<hr />
+<section class="card card-body">
+    <p>🚧 big things will be come here 🚧</p>
+</section>
+<hr />
+<section class="flex space-x-2">
+    <a class="btn btn-filled-primary" href="https://svelte.dev/" target="_blank" rel="noreferrer">Svelte</a>
+    <a class="btn btn-filled-accent" href="https://tailwindcss.com/" target="_blank" rel="noreferrer">Tailwind</a>
+    <a class="btn btn-filled-tertiary" href="https://github.com/" target="_blank" rel="noreferrer">GitHub</a>
+</section>
